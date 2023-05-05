41c90b807084480cfae5ad0e4d9be492399a9820
buraksenyurt <burakselimsenyurt@gmail.com>
2023-05-05 12:07:55 +0300

    Initiated a challenge.

---
+++ BusinessApp/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace BusinessApp;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+class CustomerLogic
+{
+    public ReturnCode CalculateBonus(Customer customer,CustomerProcessState customerProcessState){
+        throw new NotImplementedException();
+    }
+}
+
+class Customer
+{
+    public int Id { get; set; }
+    public FullName Title { get; set; } = new();
+    public int MyProperty { get; set; }
+    public CustomerType CustomerType { get; set; }
+}
+
+class FullName
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string? MidName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
+
+enum CustomerType{
+    Gold,
+    Platinium,
+    Basic,
+    Newbee
+}
+
+enum ReturnCode{
+    Success,
+    Fail
+}
+
+enum CustomerProcessState{
+    Invalid,
+    Valid,
+
+}
